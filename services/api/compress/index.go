9798208877da42601b876490c56408e7ae5b6d28
Mukarram Ali <mukarram.ali89@gmail.com>
2024-05-29 01:29:14 +0200

    feat: create copy of service in api

---
+++ services/api/compress/index.go
@@ -0,0 +1,33 @@
+package compress
+
+import (
+	"fmt"
+
+	"github.com/h2non/bimg"
+)
+
+func CompressImage(inputPath string, outputPath string, quality int, width int) error {
+	buffer, err := bimg.Read(inputPath)
+	if err != nil {
+		return err
+	}
+
+	options := bimg.Options{
+		Quality: quality,
+	}
+
+	if width > 0 {
+		options.Width = width
+	}
+
+	newImage, err := bimg.NewImage(buffer).Process(options)
+	if err != nil {
+		return fmt.Errorf("failed to process image: %v", err)
+	}
+
+	err = bimg.Write(outputPath, newImage)
+	if err != nil {
+		return fmt.Errorf("failed to write output image: %v", err)
+	}
+	return nil
+}
