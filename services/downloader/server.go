90dd311c284714a3add5d6eb2c4419f08c00c9f1
Mukarram Ali <mukarram.ali89@gmail.com>
2024-05-30 21:44:44 +0200

    feat: add downloader service

---
+++ services/downloader/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"compressor/src"
+	"net/http"
+)
+
+func init() {
+	src.RegisterQueues()
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Downloader Service is Healthy"))
+}
+
+func main() {
+	http.HandleFunc("/health", healthCheckHandler)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ services/downloader/src/queues.go
@@ -0,0 +1,44 @@
+package src
+
+import (
+	"fmt"
+	"imagestack/lib/rabbitmq_service"
+	"path/filepath"
+
+	"imagestack/lib/error_handler"
+	"imagestack/lib/file_handler"
+	"imagestack/lib/request"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+var (
+	downloadQueueService *rabbitmq_service.RabbitMqService
+	compressQueueService *rabbitmq_service.RabbitMqService
+	redisService         *request.RequestService
+)
+
+func init() {
+	downloadQueueService = rabbitmq_service.NewRabbitMqService("download_images", RABBITMQ_URL)
+	compressQueueService = rabbitmq_service.NewRabbitMqService("compress_images", RABBITMQ_URL)
+
+	redisService = request.NewRequestService(REDIS_URL)
+}
+
+func RegisterQueues() {
+	go downloadQueueService.Consume(func(msg amqp091.Delivery) {
+		requestId := string(msg.Body)
+		request, _ := redisService.GetRequest(requestId)
+
+		// download image
+		localPath := filepath.Join(BASE_IMAGE_DIR, "raw", fmt.Sprintf("%s.jpg", requestId))
+		err := file_handler.DownloadImage(request.Url, localPath)
+		error_handler.FailOnError(err, "Could not download image")
+
+		// update request with local un optimized image path
+		redisService.SetLocalPathUnOptimized(requestId, localPath)
+
+		// send event for compressing
+		compressQueueService.Publish(requestId)
+	})
+}
